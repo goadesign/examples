903f21940d41a441c1d45ab81d50241265f48db0
Raphael Simon <raphael@rightscale.com>
2016-03-20 22:00:00 -0700

    Initial commit

---
+++ graceful/app/media_types.go
@@ -0,0 +1,13 @@
+//************************************************************************//
+// API "adder": Application Media Types
+//
+// Generated with goagen v0.0.1, command line:
+// $ goagen
+// --out=$(GOPATH)/src/github.com/goadesign/examples/graceful
+// --design=github.com/goadesign/examples/graceful/design
+// --pkg=app
+//
+// The content of this file is auto-generated, DO NOT MODIFY
+//************************************************************************//
+
+package app
