46bce50ed8b5a9c4d99d8618f4b034a8c1a55c4d
Raphael Simon <simon.raphael@gmail.com>
2016-04-08 18:06:00 -0700

    Ignore adder binary

--- types/design/hash.go
+++ types/design/hash.go
@@ -1,21 +1,25 @@
 package design
 
+import (
+	. "github.com/goadesign/goa/design"
+	. "github.com/goadesign/goa/design/apidsl"
+)
+
+// Note: goa supports hash types with arbitrary types as key. However JSON and therefore Swagger
+// do not allow for anything but strings as keys. This example generates Swagger so only uses
+// hashes with string keys. Just know that if you don't use JSON to serialize requests or
+// responses you can use hashes with keys of any type.
 var _ = Resource("hash_prism", func() {
 	BasePath("/hash")
 	Description("Resource hash_prism contains artifical actions that showcase the use of the hash type in the DSL.")
-	Action("val_show", func() {
-		Description("Action val_show returns a media type with one hash member per primitive type, each using the primitive type as value and String as key")
-		Routing(GET("/val"))
-		Response(OK, HashValPrismMedia)
+	Action("show", func() {
+		Description("Action show returns a media type with one hash member per primitive type, each using the primitive type as value and String as key")
+		Routing(GET(""))
+		Response(OK, HashPrismMedia)
 	})
-	Action("key_show", func() {
-		Description("Action key_show returns a media type with one hash member per primitive type, each using the primitive type as key and String as value")
-		Routing(GET("/key"))
-		Response(OK, HashKeyPrismMedia)
-	})
-	Action("val_create", func() {
-		Description("Action val_create accepts a payload with one hash member per primitive type, each using the type as value and String as key")
-		Routing(POST("/val"))
+	Action("create", func() {
+		Description("Action create accepts a payload with one hash member per primitive type, each using the type as value and String as key")
+		Routing(POST(""))
 		Payload(func() {
 			Member("bool_val_hash", HashOf(String, Boolean), "Hash with Boolean value member")
 			Member("int_val_hash", HashOf(String, Integer), "Hash with Integer value member")
@@ -25,28 +29,16 @@ var _ = Resource("hash_prism", func() {
 			Member("any_val_hash", HashOf(String, Any), "Hash with Any value member")
 		})
 	})
-	Action("key_create", func() {
-		Description("Action key_create accepts a payload with one hash member per primitive type, each using the type as key and String as value")
-		Routing(POST("/key"))
-		Payload(func() {
-			Member("bool_key_hash", HashOf(Boolean, String), "Hash with Boolean key member")
-			Member("int_key_hash", HashOf(Integer, String), "Hash with Integer key member")
-			Member("num_key_hash", HashOf(Number, String), "Hash with Number key member")
-			Member("String_key_hash", HashOf(String, String), "Hash with String key member")
-			Member("date_key_time_hash", HashOf(DateTime, String), "Hash with DateTime key member")
-			Member("any_key_hash", HashOf(Any, String), "Hash with Any key member")
-		})
-	})
 })
 
-var HashValPrismMedia = MediaType("vnd.goadesign.examples.valprism", func() {
-	Description("HashValPrismMedia is a media type with one hash member per primitive type, each using the type as value and String as key")
+var HashPrismMedia = MediaType("vnd.goadesign.examples.hashprism", func() {
+	Description("HashPrismMedia is a media type with one hash member per primitive type, each using the type as value and String as key")
 	Attributes(func() {
 		Attribute("bool_val_hash", HashOf(String, Boolean), "Hash with Boolean value member")
 		Attribute("int_val_hash", HashOf(String, Integer), "Hash with Integer value member")
 		Attribute("num_val_hash", HashOf(String, Number), "Hash with Number value member")
 		Attribute("String_val_hash", HashOf(String, String), "Hash with String value member")
-		Attribute("date_val_time_hash", HashOf(String, DateTime), "Hash with DateTime value member")
+		Attribute("date_time_val_hash", HashOf(String, DateTime), "Hash with DateTime value member")
 		Attribute("any_val_hash", HashOf(String, Any), "Hash with Any value member")
 	})
 	View("default", func() {
@@ -56,25 +48,5 @@ var HashValPrismMedia = MediaType("vnd.goadesign.examples.valprism", func() {
 		Attribute("String_val_hash")
 		Attribute("date_time_val_hash")
 		Attribute("any_val_hash")
-	})
-})
-
-var HashKeyPrismMedia = MediaType("vnd.goadesign.examples.keyprism", func() {
-	Description("HashKeyPrismMedia is a media type with one hash member per primitive type, each using the type as key and String as value")
-	Attributes(func() {
-		Attribute("bool_key_hash", HashOf(Boolean, String), "Hash with Boolean key member")
-		Attribute("int_key_hash", HashOf(Integer, String), "Hash with Integer key member")
-		Attribute("num_key_hash", HashOf(Number, String), "Hash with Number key member")
-		Attribute("String_key_hash", HashOf(String, String), "Hash with String key member")
-		Attribute("date_key_time_hash", HashOf(DateTime, String), "Hash with DateTime key member")
-		Attribute("any_key_hash", HashOf(Any, String), "Hash with Any key member")
-	})
-	View("default", func() {
-		Attribute("bool_key_hash")
-		Attribute("int_key_hash")
-		Attribute("num_key_hash")
-		Attribute("String_key_hash")
-		Attribute("date_time_key_hash")
-		Attribute("any_key_hash")
 	})
 })
--- types/generate.go
+++ types/generate.go
@@ -1,7 +1,7 @@
-//go:generate goagen -d github.com/goadesign/examples/design app
-//go:generate goagen -d github.com/goadesign/examples/design main
-//go:generate goagen -d github.com/goadesign/examples/design client
-//go:generate goagen -d github.com/goadesign/examples/design swagger
-//go:generate goagen -d github.com/goadesign/examples/design js
-//go:generate goagen -d github.com/goadesign/examples/design schema
-package generate
+//go:generate goagen -d github.com/goadesign/examples/types/design app
+//go:generate goagen -d github.com/goadesign/examples/types/design main
+//go:generate goagen -d github.com/goadesign/examples/types/design client
+//go:generate goagen -d github.com/goadesign/examples/types/design swagger
+//go:generate goagen -d github.com/goadesign/examples/types/design js
+//go:generate goagen -d github.com/goadesign/examples/types/design schema
+package main
