f83bac40e02d1d499d133976b698b2125aff835b
Raphael Simon <simon.raphael@gmail.com>
2016-05-05 21:00:11 -0700

    Address code review comments.

--- security/design/jwt.go
+++ security/design/jwt.go
@@ -6,17 +6,19 @@ import (
 )
 
 // JWT defines a security scheme using JWT.  The scheme uses the "Authorization" header to lookup
-// the token.  It also defines a required scope "api".
+// the token.  It also defines then scope "api".
 var JWT = JWTSecurity("jwt", func() {
 	Header("Authorization")
-	Scope("api", "API access")
+	Scope("api", "API access") // Define "api" scope
 })
 
+// Resource JWTEndpoints uses the JWTSecurity security scheme.
 var _ = Resource("JWTEndpoints", func() {
 	Description("This resource uses JWT to secure its endpoints")
 	DefaultMedia(SuccessMedia)
 
-	Security(JWT)
+	Security(JWT) // Use JWT to auth requests to this endpoint
+	Scope("api")  // Enforce presence of "api" scope in JWT claims.
 
 	Action("secured", func() {
 		Description("This action is secured with the jwt scheme")
@@ -28,7 +30,7 @@ var _ = Resource("JWTEndpoints", func() {
 	Action("unsecured", func() {
 		Description("This action does not require auth")
 		Routing(GET("/jwt/unsecured"))
-		NoSecurity()
+		NoSecurity() // Override the need for auth
 		Response(OK)
 	})
 })
