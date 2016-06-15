6db80e550afd50a3166b72e264bea53de6fba098
Raphael Simon <simon.raphael@gmail.com>
2016-06-14 23:09:51 -0700

    Add OAuth2 example

--- security/oauth2.go
+++ security/oauth2.go
@@ -14,12 +14,6 @@ type OAuth2Controller struct {
 	*goa.Controller
 }
 
-const (
-	// TheAccessToken is the only authorized bearer value for incoming request authorization
-	// headers. A real service would generate random tokens and persist them in a backend.
-	TheAccessToken = "acessme"
-)
-
 // NewOAuth2Middleware creates a middleware that checks for the presence of an authorization header
 // and validates its content.
 func NewOAuth2Middleware() goa.Middleware {
@@ -32,10 +26,10 @@ func NewOAuth2Middleware() goa.Middleware {
 				return ErrUnauthorized("missing auth header")
 			}
 			tok := token[0]
-			if len(tok) < 10 || !strings.HasPrefix(tok, "Bearer: ") {
+			if len(tok) < 9 || !strings.HasPrefix(tok, "Bearer ") {
 				return ErrUnauthorized("invalid auth header")
 			}
-			tok = tok[8:]
+			tok = tok[7:]
 
 			// Validate token here against value stored in DB for example
 			if tok != TheAccessToken {
@@ -53,21 +47,27 @@ func NewOAuth2Controller(service *goa.Service) *OAuth2Controller {
 
 // Secure runs the secure action.
 func (c *OAuth2Controller) Secure(ctx *app.SecureOauth2Context) error {
-	// TBD: implement
-	res := &app.Success{}
+	res := &app.Success{OK: true}
 	return ctx.OK(res)
 }
 
 // Unsecure runs the unsecure action.
 func (c *OAuth2Controller) Unsecure(ctx *app.UnsecureOauth2Context) error {
-	// TBD: implement
-	res := &app.Success{}
+	res := &app.Success{OK: true}
 	return ctx.OK(res)
 }
 
 // Write runs the write action.
 func (c *OAuth2Controller) Write(ctx *app.WriteOauth2Context) error {
-	// TBD: implement
-	res := &app.Success{}
+	res := &app.Success{OK: true}
 	return ctx.OK(res)
+}
+
+// HandleRedirect is the target of the OAuth2 authorization redirect. This endpoint would not exist
+// in your service in real life but in the 3rd party service wanting to access your service on
+// behalf of a end user. In this example we implement it so that we can see the code being received
+// and log it.
+func (c *OAuth2Controller) HandleRedirect(ctx *app.HandleRedirectOauth2Context) error {
+	goa.LogInfo(ctx, "got redirect request", "code", *ctx.Code)
+	return ctx.NoContent()
 }
