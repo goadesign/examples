9116ed91d718cdafaac97b5fd9eb8cab3853bb81
Raphael Simon <simon.raphael@gmail.com>
2016-06-26 15:15:34 -0700

    Update JWT example to lastest jwt-go package

--- security/jwt.go
+++ security/jwt.go
@@ -55,12 +55,12 @@ func (c *JWTController) Signin(ctx *app.SigninJWTContext) error {
 	// Generate JWT
 	token := jwtgo.New(jwtgo.SigningMethodRS512)
 	in10m := time.Now().Add(time.Duration(10) * time.Minute).Unix()
-	token.Claims = map[string]interface{}{
+	token.Claims = jwtgo.MapClaims{
 		"iss":    "Issuer",              // who creates the token and signs it
 		"aud":    "Audience",            // to whom the token is intended to be sent
 		"exp":    in10m,                 // time when the token will expire (10 minutes from now)
 		"jti":    uuid.NewV4().String(), // a unique identifier for the token
-		"iat":    time.Now(),            // when the token was issued/created (now)
+		"iat":    time.Now().Unix(),     // when the token was issued/created (now)
 		"nbf":    2,                     // time before which the token is not yet valid (2 minutes ago)
 		"sub":    "subject",             // the subject/principal is whom the token is about
 		"scopes": "api:access",          // token scope - not a standard claim
