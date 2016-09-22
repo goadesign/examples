507f97a1fe5b56912de79d0b127a47ecb040b2dd
Raphael Simon <simon.raphael@gmail.com>
2016-09-21 23:43:45 -0700

    Add endpoints example

---
+++ endpoints/app/security.go
@@ -0,0 +1,65 @@
+//************************************************************************//
+// API "adder": Application Security
+//
+// Generated with goagen v1.0.0, command line:
+// $ goagen
+// --design=github.com/goadesign/examples/endpoints/design
+// --out=$(GOPATH)/src/github.com/goadesign/examples/endpoints
+// --version=v1.0.0
+//
+// The content of this file is auto-generated, DO NOT MODIFY
+//************************************************************************//
+
+package app
+
+import (
+	"github.com/goadesign/goa"
+	"golang.org/x/net/context"
+	"net/http"
+)
+
+type (
+	// Private type used to store auth handler info in request context
+	authMiddlewareKey string
+)
+
+// UseAPIKeyMiddleware mounts the api_key auth middleware onto the service.
+func UseAPIKeyMiddleware(service *goa.Service, middleware goa.Middleware) {
+	service.Context = context.WithValue(service.Context, authMiddlewareKey("api_key"), middleware)
+}
+
+// NewAPIKeySecurity creates a api_key security definition.
+func NewAPIKeySecurity() *goa.APIKeySecurity {
+	def := goa.APIKeySecurity{
+		In:   goa.LocQuery,
+		Name: "key",
+	}
+	return &def
+}
+
+// UseJWTMiddleware mounts the jwt auth middleware onto the service.
+func UseJWTMiddleware(service *goa.Service, middleware goa.Middleware) {
+	service.Context = context.WithValue(service.Context, authMiddlewareKey("jwt"), middleware)
+}
+
+// NewJWTSecurity creates a jwt security definition.
+func NewJWTSecurity() *goa.OAuth2Security {
+	def := goa.OAuth2Security{
+		Flow:             "implicit",
+		TokenURL:         "",
+		AuthorizationURL: "http://localhost:8080/auth"}
+	return &def
+}
+
+// handleSecurity creates a handler that runs the auth middleware for the security scheme.
+func handleSecurity(schemeName string, h goa.Handler, scopes ...string) goa.Handler {
+	return func(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
+		scheme := ctx.Value(authMiddlewareKey(schemeName))
+		am, ok := scheme.(goa.Middleware)
+		if !ok {
+			return goa.NoAuthMiddleware(schemeName)
+		}
+		ctx = goa.WithRequiredScopes(ctx, scopes)
+		return am(h)(ctx, rw, req)
+	}
+}
---
+++ endpoints/tool/adder-cli/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/goadesign/examples/adder/client"
+	"github.com/goadesign/examples/adder/tool/cli"
+	"github.com/spf13/cobra"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	// Create command line parser
+	app := &cobra.Command{
+		Use:   "adder-cli",
+		Short: `CLI client for the adder service`,
+	}
+
+	// Create client struct
+	c := client.New(newHTTPClient())
+
+	// Register global flags
+	app.PersistentFlags().StringVarP(&c.Scheme, "scheme", "s", "", "Set the requests scheme")
+	app.PersistentFlags().StringVarP(&c.Host, "host", "H", "localhost:8080", "API hostname")
+	app.PersistentFlags().DurationVarP(&c.Timeout, "timeout", "t", time.Duration(20)*time.Second, "Set the request timeout")
+	app.PersistentFlags().BoolVar(&c.Dump, "dump", false, "Dump HTTP request and response.")
+
+	// Initialize API client
+	c.UserAgent = "adder-cli/0"
+
+	// Register API commands
+	cli.RegisterCommands(app, c)
+
+	// Execute!
+	if err := app.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(-1)
+	}
+}
+
+// newHTTPClient returns the HTTP client used by the API client to make requests to the service.
+func newHTTPClient() *http.Client {
+	// TBD: Change as needed (e.g. to use a different transport to control redirection policy or
+	// disable cert validation or...)
+	return http.DefaultClient
+}
