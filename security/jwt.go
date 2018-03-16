2e8f1645fc69948cdeb3743986ebc065bae9e479
Taichi Sasaki <tchsskk@gmail.com>
2018-03-16 10:26:47 +0900

    Fix security example

--- security/jwt.go
+++ security/jwt.go
@@ -59,14 +59,14 @@ func (c *JWTController) Signin(ctx *app.SigninJWTContext) error {
 	token := jwtgo.New(jwtgo.SigningMethodRS512)
 	in10m := time.Now().Add(time.Duration(10) * time.Minute).Unix()
 	token.Claims = jwtgo.MapClaims{
-		"iss":    "Issuer",              // who creates the token and signs it
-		"aud":    "Audience",            // to whom the token is intended to be sent
-		"exp":    in10m,                 // time when the token will expire (10 minutes from now)
-		"jti":    uuid.NewV4().String(), // a unique identifier for the token
-		"iat":    time.Now().Unix(),     // when the token was issued/created (now)
-		"nbf":    2,                     // time before which the token is not yet valid (2 minutes ago)
-		"sub":    "subject",             // the subject/principal is whom the token is about
-		"scopes": "api:access",          // token scope - not a standard claim
+		"iss":    "Issuer",                         // who creates the token and signs it
+		"aud":    "Audience",                       // to whom the token is intended to be sent
+		"exp":    in10m,                            // time when the token will expire (10 minutes from now)
+		"jti":    uuid.Must(uuid.NewV4()).String(), // a unique identifier for the token
+		"iat":    time.Now().Unix(),                // when the token was issued/created (now)
+		"nbf":    2,                                // time before which the token is not yet valid (2 minutes ago)
+		"sub":    "subject",                        // the subject/principal is whom the token is about
+		"scopes": "api:access",                     // token scope - not a standard claim
 	}
 	signedToken, err := token.SignedString(c.privateKey)
 	if err != nil {
