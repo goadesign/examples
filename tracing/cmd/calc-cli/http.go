af40d61035a77f61e6aafec727cc727fc01166dd
Raphael Simon <simon.raphael@gmail.com>
2019-03-09 21:07:01 -0800

    Update examples for Goa v2
    The previous examples have been moved to the 'v1' branch.

---
+++ tracing/cmd/calc-cli/http.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	cli "goa.design/examples/basic/gen/http/cli/calc"
+	"goa.design/goa"
+	goahttp "goa.design/goa/http"
+	"goa.design/goa/http/middleware"
+	"goa.design/goa/http/middleware/xray"
+)
+
+func doHTTP(scheme, host string, timeout int, debug bool) (goa.Endpoint, interface{}, error) {
+	var (
+		doer goahttp.Doer
+	)
+	{
+		doer = &http.Client{Timeout: time.Duration(timeout) * time.Second}
+		if debug {
+			doer = goahttp.NewDebugDoer(doer)
+		}
+		// Wrap doer with X-Ray and trace client middleware. Order is very important.
+		doer = xray.WrapDoer(doer)
+		doer = middleware.WrapDoer(doer)
+	}
+
+	return cli.ParseEndpoint(
+		scheme,
+		host,
+		doer,
+		goahttp.RequestEncoder,
+		goahttp.ResponseDecoder,
+		debug,
+	)
+}
+func httpUsageCommands() string {
+	return cli.UsageCommands()
+}
+
+func httpUsageExamples() string {
+	return cli.UsageExamples()
+}
