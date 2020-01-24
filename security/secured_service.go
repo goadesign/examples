b2dfd802ab91ce0431e396a6337a4d4ccec52041
akm <akm2000@gmail.com>
2020-01-25 00:43:48 +0900

    rm -rf cmd/ secured_service.go auth.go && goa example goa.design/examples/security/design

--- security/secured_service.go
+++ security/secured_service.go
@@ -4,75 +4,125 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"time"
 
-	jwt "github.com/dgrijalva/jwt-go"
 	securedservice "goa.design/examples/security/gen/secured_service"
+	"goa.design/goa/v3/security"
 )
 
 // secured_service service example implementation.
 // The example methods log the requests and return zero values.
-type securedServiceSvc struct {
+type securedServicesrvc struct {
 	logger *log.Logger
 }
 
 // NewSecuredService returns the secured_service service implementation.
 func NewSecuredService(logger *log.Logger) securedservice.Service {
-	return &securedServiceSvc{logger}
+	return &securedServicesrvc{logger}
 }
 
-// Creates a valid JWT after authenticating using basic_auth scheme.
-func (s *securedServiceSvc) Signin(ctx context.Context, p *securedservice.SigninPayload) (res *securedservice.Creds, err error) {
-	// create JWT token
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"nbf":    time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
-		"iat":    time.Now().Unix(),
-		"scopes": []string{"api:read", "api:write"},
-	})
+// BasicAuth implements the authorization logic for service "secured_service"
+// for the "basic" security scheme.
+func (s *securedServicesrvc) BasicAuth(ctx context.Context, user, pass string, scheme *security.BasicScheme) (context.Context, error) {
+	//
+	// TBD: add authorization logic.
+	//
+	// In case of authorization failure this function should return
+	// one of the generated error structs, e.g.:
+	//
+	//    return ctx, myservice.MakeUnauthorizedError("invalid token")
+	//
+	// Alternatively this function may return an instance of
+	// goa.ServiceError with a Name field value that matches one of
+	// the design error names, e.g:
+	//
+	//    return ctx, goa.PermanentError("unauthorized", "invalid token")
+	//
+	return ctx, fmt.Errorf("not implemented")
+}
 
-	s.logger.Printf("user '%s' logged in", p.Username)
+// JWTAuth implements the authorization logic for service "secured_service" for
+// the "jwt" security scheme.
+func (s *securedServicesrvc) JWTAuth(ctx context.Context, token string, scheme *security.JWTScheme) (context.Context, error) {
+	//
+	// TBD: add authorization logic.
+	//
+	// In case of authorization failure this function should return
+	// one of the generated error structs, e.g.:
+	//
+	//    return ctx, myservice.MakeUnauthorizedError("invalid token")
+	//
+	// Alternatively this function may return an instance of
+	// goa.ServiceError with a Name field value that matches one of
+	// the design error names, e.g:
+	//
+	//    return ctx, goa.PermanentError("unauthorized", "invalid token")
+	//
+	return ctx, fmt.Errorf("not implemented")
+}
 
-	// note that if "SignedString" returns an error then it is returned as
-	// an internal error to the client
-	t, err := token.SignedString(Key)
-	if err != nil {
-		return nil, err
-	}
-	return &securedservice.Creds{
-		JWT:        t,
-		OauthToken: t,
-		APIKey:     "my_awesome_api_key",
-	}, nil
+// APIKeyAuth implements the authorization logic for service "secured_service"
+// for the "api_key" security scheme.
+func (s *securedServicesrvc) APIKeyAuth(ctx context.Context, key string, scheme *security.APIKeyScheme) (context.Context, error) {
+	//
+	// TBD: add authorization logic.
+	//
+	// In case of authorization failure this function should return
+	// one of the generated error structs, e.g.:
+	//
+	//    return ctx, myservice.MakeUnauthorizedError("invalid token")
+	//
+	// Alternatively this function may return an instance of
+	// goa.ServiceError with a Name field value that matches one of
+	// the design error names, e.g:
+	//
+	//    return ctx, goa.PermanentError("unauthorized", "invalid token")
+	//
+	return ctx, fmt.Errorf("not implemented")
+}
+
+// OAuth2Auth implements the authorization logic for service "secured_service"
+// for the "oauth2" security scheme.
+func (s *securedServicesrvc) OAuth2Auth(ctx context.Context, token string, scheme *security.OAuth2Scheme) (context.Context, error) {
+	//
+	// TBD: add authorization logic.
+	//
+	// In case of authorization failure this function should return
+	// one of the generated error structs, e.g.:
+	//
+	//    return ctx, myservice.MakeUnauthorizedError("invalid token")
+	//
+	// Alternatively this function may return an instance of
+	// goa.ServiceError with a Name field value that matches one of
+	// the design error names, e.g:
+	//
+	//    return ctx, goa.PermanentError("unauthorized", "invalid token")
+	//
+	return ctx, fmt.Errorf("not implemented")
+}
+
+// Creates a valid JWT
+func (s *securedServicesrvc) Signin(ctx context.Context, p *securedservice.SigninPayload) (res *securedservice.Creds, err error) {
+	res = &securedservice.Creds{}
+	s.logger.Print("securedService.signin")
+	return
 }
 
 // This action is secured with the jwt scheme
-func (s *securedServiceSvc) Secure(ctx context.Context, p *securedservice.SecurePayload) (res string, err error) {
-	res = fmt.Sprintf("User authorized using JWT token %q", p.Token)
-	s.logger.Printf(res)
-	if p.Fail != nil && *p.Fail {
-		s.logger.Printf("Uh oh! `fail` passed in parameter. Auth failed!")
-		return "", securedservice.Unauthorized("forced authentication failure")
-	}
+func (s *securedServicesrvc) Secure(ctx context.Context, p *securedservice.SecurePayload) (res string, err error) {
+	s.logger.Print("securedService.secure")
 	return
 }
 
 // This action is secured with the jwt scheme and also requires an API key
 // query string.
-func (s *securedServiceSvc) DoublySecure(ctx context.Context, p *securedservice.DoublySecurePayload) (res string, err error) {
-	res = fmt.Sprintf("User authorized using JWT token %q and API Key %q", p.Token, p.Key)
-	s.logger.Printf(res)
+func (s *securedServicesrvc) DoublySecure(ctx context.Context, p *securedservice.DoublySecurePayload) (res string, err error) {
+	s.logger.Print("securedService.doubly_secure")
 	return
 }
 
 // This action is secured with the jwt scheme and also requires an API key
 // header.
-func (s *securedServiceSvc) AlsoDoublySecure(ctx context.Context, p *securedservice.AlsoDoublySecurePayload) (res string, err error) {
-	if p.Username != nil && p.Password != nil && p.OauthToken != nil {
-		res = fmt.Sprintf("User authorized using username %q/password %q and OAuth2 token %q", *p.Username, *p.Password, *p.OauthToken)
-		s.logger.Printf(res)
-		return
-	}
-	res = fmt.Sprintf("User authorized using JWT token %q and API Key %q", *p.Token, *p.Key)
-	s.logger.Print(res)
+func (s *securedServicesrvc) AlsoDoublySecure(ctx context.Context, p *securedservice.AlsoDoublySecurePayload) (res string, err error) {
+	s.logger.Print("securedService.also_doubly_secure")
 	return
 }
