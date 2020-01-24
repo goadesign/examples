5280cb9fc679be99de8911d819466e27a85e9ea6
akm <akm2000@gmail.com>
2020-01-25 00:45:55 +0900

    revert implementation

--- security/secured_service.go
+++ security/secured_service.go
@@ -102,27 +102,56 @@ func (s *securedServicesrvc) OAuth2Auth(ctx context.Context, token string, schem
 
 // Creates a valid JWT
 func (s *securedServicesrvc) Signin(ctx context.Context, p *securedservice.SigninPayload) (res *securedservice.Creds, err error) {
-	res = &securedservice.Creds{}
-	s.logger.Print("securedService.signin")
-	return
+	// create JWT token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"nbf":    time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+		"iat":    time.Now().Unix(),
+		"scopes": []string{"api:read", "api:write"},
+	})
+
+	s.logger.Printf("user '%s' logged in", p.Username)
+
+	// note that if "SignedString" returns an error then it is returned as
+	// an internal error to the client
+	t, err := token.SignedString(Key)
+	if err != nil {
+		return nil, err
+	}
+	return &securedservice.Creds{
+		JWT:        t,
+		OauthToken: t,
+		APIKey:     "my_awesome_api_key",
+	}, nil
 }
 
 // This action is secured with the jwt scheme
 func (s *securedServicesrvc) Secure(ctx context.Context, p *securedservice.SecurePayload) (res string, err error) {
-	s.logger.Print("securedService.secure")
+	res = fmt.Sprintf("User authorized using JWT token %q", p.Token)
+	s.logger.Printf(res)
+	if p.Fail != nil && *p.Fail {
+		s.logger.Printf("Uh oh! `fail` passed in parameter. Auth failed!")
+		return "", securedservice.Unauthorized("forced authentication failure")
+	}
 	return
 }
 
 // This action is secured with the jwt scheme and also requires an API key
 // query string.
 func (s *securedServicesrvc) DoublySecure(ctx context.Context, p *securedservice.DoublySecurePayload) (res string, err error) {
-	s.logger.Print("securedService.doubly_secure")
+	res = fmt.Sprintf("User authorized using JWT token %q and API Key %q", p.Token, p.Key)
+	s.logger.Printf(res)
 	return
 }
 
 // This action is secured with the jwt scheme and also requires an API key
 // header.
 func (s *securedServicesrvc) AlsoDoublySecure(ctx context.Context, p *securedservice.AlsoDoublySecurePayload) (res string, err error) {
-	s.logger.Print("securedService.also_doubly_secure")
+	if p.Username != nil && p.Password != nil && p.OauthToken != nil {
+		res = fmt.Sprintf("User authorized using username %q/password %q and OAuth2 token %q", *p.Username, *p.Password, *p.OauthToken)
+		s.logger.Printf(res)
+		return
+	}
+	res = fmt.Sprintf("User authorized using JWT token %q and API Key %q", *p.Token, *p.Key)
+	s.logger.Print(res)
 	return
 }
