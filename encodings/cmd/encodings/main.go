f59c5027362967612944f8571e465c52ccf1a9be
Raphael Simon <simon.raphael@gmail.com>
2020-07-11 00:07:10 -0700

    Update to latest Goa

--- encodings/cmd/encodings/main.go
+++ encodings/cmd/encodings/main.go
@@ -10,6 +10,7 @@ import (
 	"os/signal"
 	"strings"
 	"sync"
+	"syscall"
 
 	encodings "goa.design/examples/encodings"
 	text "goa.design/examples/encodings/gen/text"
@@ -60,7 +61,7 @@ func main() {
 	// that SIGINT and SIGTERM signals cause the services to stop gracefully.
 	go func() {
 		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 		errc <- fmt.Errorf("%s", <-c)
 	}()
 
--- multipart/cmd/resume/http.go
+++ multipart/cmd/resume/http.go
@@ -94,7 +94,7 @@ func handleHTTPServer(ctx context.Context, u *url.URL, resumeEndpoints *resume.E
 		logger.Printf("shutting down HTTP server at %q", u.Host)
 
 		// Shutdown gracefully with a 30s timeout.
-		ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 		defer cancel()
 
 		srv.Shutdown(ctx)
