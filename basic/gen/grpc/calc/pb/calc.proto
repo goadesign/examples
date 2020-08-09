fc408a6b5283e9b482767c3249d59dc3e4a4fc05
Raphael Simon <simon.raphael@gmail.com>
2020-08-08 22:07:24 -0700

    Release v3.2.1

--- basic/gen/grpc/calc/pb/calc.proto
+++ basic/gen/grpc/calc/pb/calc.proto
@@ -1,4 +1,4 @@
-// Code generated with goa v3.2.0, DO NOT EDIT.
+// Code generated with goa v3.2.1, DO NOT EDIT.
 //
 // calc protocol buffer definition
 //
--- security/gen/grpc/secured_service/pb/secured_service.proto
+++ security/gen/grpc/secured_service/pb/secured_service.proto
@@ -1,4 +1,4 @@
-// Code generated with goa v3.2.0, DO NOT EDIT.
+// Code generated with goa v3.2.1, DO NOT EDIT.
 //
 // secured_service protocol buffer definition
 //
