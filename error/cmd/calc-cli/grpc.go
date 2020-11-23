633286f338988a1913602483e05a1abea315f6c8
Raphael Simon <simon.raphael@gmail.com>
2020-11-22 19:59:41 -0800

    Update error example
    Illustrate how to override default error responses.

---
+++ error/cmd/calc-cli/grpc.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	cli "goa.design/examples/error/gen/grpc/cli/calc"
+	goa "goa.design/goa/v3/pkg"
+	"google.golang.org/grpc"
+)
+
+func doGRPC(scheme, host string, timeout int, debug bool) (goa.Endpoint, interface{}, error) {
+	conn, err := grpc.Dial(host, grpc.WithInsecure())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not connect to gRPC server at %s: %v\n", host, err)
+	}
+	return cli.ParseEndpoint(conn)
+}
